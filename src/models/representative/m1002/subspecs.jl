71f7e11588c14598993f6a111d884f54f4448905
Shlok Goyal <shlok.goyal@gmail.com>
2021-11-19 15:30:48 -0500

    Update subspecs for new quarter

--- src/models/representative/m1002/subspecs.jl
+++ src/models/representative/m1002/subspecs.jl
@@ -1869,7 +1869,7 @@ function ss64!(m::Model1002)
     m <= Setting(:regime_dates, Dict{Int, Date}(1 => date_presample_start(m), 2 => Date(2020, 3, 31),
                                                 3 => Date(2020, 6, 30), 4 => Date(2020, 9, 30),
                                                 5 => Date(2020, 12, 31), 6 => Date(2021, 3, 31),
-                                                7 => Date(2021, 6, 30)))
+                                                7 => Date(2021, 6, 30), 8 => Date(2021, 9, 30)))
     m <= Setting(:time_varying_trends, true)
     setup_regime_switching_inds!(m)
 
@@ -6161,7 +6161,7 @@ function ss84!(m::Model1002)
     m <= Setting(:regime_dates, Dict{Int, Date}(1 => date_presample_start(m), 2 => Date(2020, 3, 31),
                                                 3 => Date(2020, 6, 30), 4 => Date(2020, 9, 30),
                                                 5 => Date(2020, 12, 31), 6 => Date(2021, 3, 31),
-                                                7 => Date(2021, 6, 30)))
+                                                7 => Date(2021, 6, 30), 8 => Date(2021, 9, 30)))
     m <= Setting(:time_varying_trends, true)
     setup_regime_switching_inds!(m)
 
@@ -6369,7 +6369,7 @@ function ss85!(m::Model1002)
     m <= Setting(:regime_dates, Dict{Int, Date}(1 => date_presample_start(m), 2 => Date(2020, 3, 31),
                                                 3 => Date(2020, 6, 30), 4 => Date(2020, 9, 30),
                                                 5 => Date(2020, 12, 31), 6 => Date(2021, 3, 31),
-                                                7 => Date(2021, 6, 30)))
+                                                7 => Date(2021, 6, 30), 8 => Date(2021, 9, 30)))
     m <= Setting(:time_varying_trends, true)
     setup_regime_switching_inds!(m)
 
@@ -6598,9 +6598,9 @@ end
 
 
 function add_meas_pi!(m)
-    get_setting(m, :model2para_regime)[:ρ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    get_setting(m, :model2para_regime)[:σ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    for i in 8:get_setting(m, :n_regimes)
+    get_setting(m, :model2para_regime)[:ρ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2, 8 => 2)
+    get_setting(m, :model2para_regime)[:σ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2, 8 => 2)
+    for i in 9:get_setting(m, :n_regimes)
         get_setting(m, :model2para_regime)[:ρ_meas_π][i] = 1
         get_setting(m, :model2para_regime)[:σ_meas_π][i] = 1
     end
@@ -6628,9 +6628,9 @@ end
 
 function add_zero_meas_pi!(m)
     # Set measurement errors from ss87 to 0
-    get_setting(m, :model2para_regime)[:ρ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    get_setting(m, :model2para_regime)[:σ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    for i in 8:get_setting(m, :n_regimes)
+    get_setting(m, :model2para_regime)[:ρ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2, 8 => 2)
+    get_setting(m, :model2para_regime)[:σ_meas_π] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2, 8 => 2)
+    for i in 9:get_setting(m, :n_regimes)
         get_setting(m, :model2para_regime)[:ρ_meas_π][i] = 1
         get_setting(m, :model2para_regime)[:σ_meas_π][i] = 1
     end
@@ -6676,9 +6676,9 @@ function rm_iid_pce_meas_err!(m)
 
     #get_setting(m, :model2para_regime)[:ρ_gdpdef] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
     #get_setting(m, :model2para_regime)[:σ_gdpdef] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    get_setting(m, :model2para_regime)[:ρ_corepce] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    get_setting(m, :model2para_regime)[:σ_corepce] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2)
-    for i in 8:get_setting(m, :n_regimes)
+    get_setting(m, :model2para_regime)[:ρ_corepce] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2, 8 => 2)
+    get_setting(m, :model2para_regime)[:σ_corepce] = Dict(1 => 1, 2 => 2, 3 => 2, 4 => 2, 5 => 2, 6 => 2, 7 => 2, 8 => 2)
+    for i in 9:get_setting(m, :n_regimes)
         #get_setting(m, :model2para_regime)[:ρ_gdpdef][i] = 1
         #get_setting(m, :model2para_regime)[:σ_gdpdef][i] = 1
         get_setting(m, :model2para_regime)[:ρ_corepce][i] = 1
