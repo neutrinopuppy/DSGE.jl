7e434275d55156afbc63422e1343e1a1fc3c3a9f
Alissa Johnson <alissa@oneotaslopes.org>
2022-01-21 03:10:41 -0500

    Use 60 horizons to match saved output.

--- test/analysis/df_to_table.jl
+++ test/analysis/df_to_table.jl
@@ -32,6 +32,7 @@ output_vars = add_requisite_output_vars([:histpseudo, :histobs, :histstdshocks,
 
 Random.seed!(47)
 df = CSV.read("$path/../reference/input_data/data/data_dsid=00_vint=REF.csv", DataFrame)
+m <= Setting(:forecast_horizons, 60)
 forecast_one(m, :full, :none, output_vars, df = df, verbose = :none)
 compute_meansbands(m, :full, :none, [:histobs, :forecastobs, :histpseudo, :forecastpseudo,
                                      :hist4qobs, :forecast4qobs, :hist4qpseudo, :forecast4qpseudo, :shockdecobs, :irfobs], df = df, verbose = :none)
