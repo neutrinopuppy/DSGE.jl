6301569aabb415db46daac16f9085e22e5e287ea
Shlok Goyal <shlok.goyal@gmail.com>
2022-02-18 13:29:53 -0500

    Add options for column, aggregation method in SPD data

--- src/data/transform_data.jl
+++ src/data/transform_data.jl
@@ -192,24 +192,52 @@ end
 
 """
 ```
-function transform_spd_data(spd_df::DataFrame)
+function transform_spd_data(spd_df::DataFrame; column::Symbol = :MODAL_MEDIAN,
+    use_last_survey::Bool = true, use_last_meeting::Bool = true)
 ```
 Transform the raw SPD Modal Path data into a format matching that of OIS.
 Two different forecasts of the same quarter taken at different times in the same quarter are averaged.
 FFR forecasts of different FOMC meetings in the same quarter are aggregated using the implied daily FFR.
-But if data given for quarter rather than meeting, use that value for whole quarter
+But if data given for quarter rather than meeting, use that value for whole quarter.
+
+### Inputs
+
+- `spd_df`: Modal forecasts dataset from the NY Fed's SPD.
+
+### Keyword Arguments
+
+- `column`: Which column's values to return (median, 25th, or 75th percentile)
+- `use_last_survey`: whether to use the last survey in a quarter.
+    If false, both surveys in a quarter are averaged.
+- `use_last_meeting`: whether to use the last meeting in a quarter for the forecast.
+    If false, the forecasted FFR comes from the implied daily FFR.
+- `remove_zlb`: whether to remove instances when the median forecast is at the ZLB.
 """
-function transform_spd_data(spd_df::DataFrame)
+function transform_spd_data(df::DataFrame; column::Symbol = :MODAL_MEDIAN,
+                            use_last_survey::Bool = true, use_last_meeting::Bool = true,
+                            remove_zlb::Bool = true)
+    # Copy to ensure we don't change spd_df
+    spd_df = copy(df)
+
     # Get Quarter forecast is made in
     str_len = length.(spd_df[!,:SURVEY_MTG_DT])
     start_qtr = SubString.(spd_df[!,:SURVEY_MTG_DT], 1, str_len .- 2) .* "20" .*
         SubString.(spd_df[!,:SURVEY_MTG_DT], str_len .- 1, str_len)
     start_qtr = Date.(start_qtr, "d-u-y")
+    insertcols!(spd_df, :survey_date => start_qtr)
     insertcols!(spd_df, :date => DSGE.quartertodate.(string.(year.(start_qtr)) .* "Q" .* string.(DSGE.datetoquarter.(start_qtr))))
 
     # Take avg of modal forecasts made within a quarter
-    gd = groupby(spd_df, [:date, :PREDICTION_HORIZON_DATE])
-    new_df = combine(gd, :MODAL_MEDIAN => mean, renamecols = false)
+    if use_last_survey
+        sort!(spd_df, [:survey_date])
+        gd = groupby(spd_df, :date)
+        last_surveys = combine(gd, :survey_date => (x -> x[end]), renamecols = false)
+
+        new_df = subset(spd_df, :survey_date => ByRow((x -> x in last_surveys[!,:survey_date])))
+    else
+        gd = groupby(spd_df, [:date, :PREDICTION_HORIZON_DATE])
+        new_df = combine(gd, column => mean, renamecols = false)
+    end
 
     # Get how many qtrs ahead a forecast is
     predicted_qtr = Vector{Date}(undef,nrow(new_df))
@@ -232,15 +260,14 @@ function transform_spd_data(spd_df::DataFrame)
     insertcols!(new_df, :predicted_qtr => predicted_qtr)
     insertcols!(new_df, :qtrs_ahead => DSGE.subtract_quarters.(predicted_qtr, new_df[!,:date]))
     ## insertcols!(new_df, :prev_meeting => vcat(new_df[1,:PREDICTION_HORIZON_DATE], new_df[1:end-1,:PREDICTION_HORIZON_DATE])) ## Only used when 2 qtrs_ahead rows for qtr, note 1st row never used
-    insertcols!(new_df, :final_vals => new_df[!,:MODAL_MEDIAN])
+    insertcols!(new_df, :final_vals => new_df[!,column])
 
     sort!(new_df, [:date, :qtrs_ahead])
-    insertcols!(new_df, :prev_val => vcat(new_df[1,:MODAL_MEDIAN], new_df[1:end-1,:MODAL_MEDIAN]))
+    insertcols!(new_df, :prev_val => vcat(new_df[1,column], new_df[1:end-1,column]))
 
     # Get last FOMC meeting date in qtrs when needed
     insertcols!(new_df, :last_meeting => new_df[!,:predicted_qtr])
     find_date_qtr = groupby(new_df, :predicted_qtr)
-    #last_meeting = Vector{Date}(undef, length(find_date_qtr))
     for i in 1:length(find_date_qtr)
         date_given = findall(x -> occursin.("/", x), find_date_qtr[i][!,:PREDICTION_HORIZON_DATE])
         find_date_qtr[i][!,:last_meeting] .= if !isempty(date_given)
@@ -252,7 +279,11 @@ function transform_spd_data(spd_df::DataFrame)
     end
 
     # Combine forecasts that are the same i qtrs ahead from same start qtr
-    ## Aggregate using implied daily FFR
+    ## Aggregate using implied daily FFR if !use_last_meeting
+    if use_last_meeting
+        new_df_inds = findall(i -> !occursin("/", new_df[i, :PREDICTION_HORIZON_DATE]) || new_df[i,:last_meeting] == Date(new_df[i,:PREDICTION_HORIZON_DATE], "m/d/y"), 1:nrow(new_df))
+        new_df2 = new_df[new_df_inds,:]
+    else
     gd2 = groupby(new_df, [:date, :qtrs_ahead])
     for i in 1:length(gd2)
         if nrow(gd2[i]) > 1 && all(occursin.("/", gd2[i][!,:PREDICTION_HORIZON_DATE]))
@@ -260,8 +291,8 @@ function transform_spd_data(spd_df::DataFrame)
 
             fomc_diffs = (horizon_dates[2:end] .- horizon_dates[1:end-1]) ./ Dates.Day(1)#(Date.(gd2[i][2:end,:PREDICTION_HORIZON_DATE], "m/d/y") .- Date.(gd2[i][1:end-1,:PREDICTION_HORIZON_DATE], "m/d/y")) ./ Dates.Day(1)
 
-            gd2[i][!,:final_vals] .= (sum(fomc_diffs .* gd2[i][1:end-1,:MODAL_MEDIAN]) +
-                                      (gd2[i][end,:predicted_qtr] - horizon_dates[end]) / Dates.Day(1) * gd2[i][end,:MODAL_MEDIAN] +
+            gd2[i][!,:final_vals] .= (sum(fomc_diffs .* gd2[i][1:end-1,column]) +
+                                      (gd2[i][end,:predicted_qtr] - horizon_dates[end]) / Dates.Day(1) * gd2[i][end,column] +
                                       (horizon_dates[1] - DSGE.iterate_quarters(gd2[i][1,:predicted_qtr],-1)) / Dates.Day(1) * gd2[i][1,:prev_val]) /
                                       ((gd2[i][1,:predicted_qtr] - DSGE.iterate_quarters(gd2[i][1,:predicted_qtr], -1)) / Dates.Day(1))
 
@@ -273,15 +304,15 @@ function transform_spd_data(spd_df::DataFrame)
                     string.(day.(gd2[i][no_date,:last_meeting])) .* "/" .* string.(year.(gd2[i][no_date,:last_meeting]))
 
                 ## Average those of same date
-                subgd = combine(groupby(gd2[i], :PREDICTION_HORIZON_DATE), :MODAL_MEDIAN => mean,
+                subgd = combine(groupby(gd2[i], :PREDICTION_HORIZON_DATE), column => mean,
                                 :predicted_qtr => (x -> x[1]), :prev_val => (x -> x[1]))
 
                 ## Get daily aggregated value
                 horizon_dates = Date.(subgd[!,:PREDICTION_HORIZON_DATE], "m/d/y")
                 fomc_diffs = (horizon_dates[2:end] .- horizon_dates[1:end-1]) ./ Dates.Day(1)
 
-                final_val = (sum(fomc_diffs .* subgd[1:end-1,:MODAL_MEDIAN_mean]) +
-                             (subgd[end,:predicted_qtr_function] - horizon_dates[end]) / Dates.Day(1) * subgd[end,:MODAL_MEDIAN_mean] +
+                final_val = (sum(fomc_diffs .* subgd[1:end-1,Symbol(column,:_mean)]) +
+                             (subgd[end,:predicted_qtr_function] - horizon_dates[end]) / Dates.Day(1) * subgd[end,Symbol(column,:_mean)] +
                              (horizon_dates[1] - DSGE.iterate_quarters(subgd[1,:predicted_qtr_function],-1)) / Dates.Day(1) * subgd[1,:prev_val_function]) /
                              ((subgd[1,:predicted_qtr_function] - DSGE.iterate_quarters(subgd[1,:predicted_qtr_function], -1)) / Dates.Day(1))
 
@@ -293,6 +324,7 @@ function transform_spd_data(spd_df::DataFrame)
         end
     end
     new_df2 = combine(gd2, :final_vals => mean, renamecols = false)
+    end
 
     # Widen dataframe so there's a separate column for each qtr ahead.
     new_df2 = unstack(new_df2, :date, :qtrs_ahead, :final_vals)
@@ -302,7 +334,24 @@ function transform_spd_data(spd_df::DataFrame)
     # Sort and rename columns
     col_inds = sortperm(parse.(Int,names(new_df2)[2:end]))
     new_df2 = new_df2[:,vcat(1,col_inds .+ 1)]
-    names(new_df2)[2:end] = "exp_ant" .* names(new_df2)[2:end]
+    rename!(new_df2, vcat(names(new_df2)[1], "exp_ant" .* names(new_df2)[2:end]))
+
+    # Remove periods when median forecast at ZLB if required
 
     return new_df2
+end
+
+"""
+```
+function spd_data_bands(spd_df::DataFrame)
+```
+Transform the raw SPD Modal Path data into a format matching that of OIS.
+Returns 3 datasets, one each for 25%, 50%, and 75%.
+"""
+function spd_data_bands(spd_df::DataFrame)
+    df_med = transform_spd_data(spd_df, column = :MODAL_MEDIAN)
+    df_25 = transform_spd_data(spd_df, column = :MODAL_25TH)
+    df_75 = transform_spd_data(spd_df, column = :MODAL_75TH)
+
+    return df_med, df_25, df_75
 end
