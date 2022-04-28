4a7493b14f553b0e3057a0ebc935e1e72d3deb65
aidan-cg <41960733+aidan-cg@users.noreply.github.com>
2022-04-28 15:52:56 -0400

    Fix conditional mix-up relating to forecast start date

--- src/forecast/forecast.jl
+++ src/forecast/forecast.jl
@@ -267,13 +267,13 @@ function forecast(m::AbstractDSGEModel, system::RegimeSwitchingSystem{S}, z0::Ve
     n_fcast_reg = get_setting(m, :n_fcast_regimes)
     if cond_type != :none
         if :obs_nominalrate in cond_full_names(m)
-            n_fcast_reg -= (get_setting(m, :reg_post_conditional_end)-1) - # Remove number of regimes switched since starting
+            n_fcast_reg -= (get_setting(m, :reg_post_conditional_end)) - # Remove number of regimes switched since starting
                 get_setting(m, :reg_forecast_start)                    # the conditional forecast
             if n_fcast_reg == 0 # Then no new regimes after conditional forecast ends
                 n_fcast_reg = 1 # So we use the last conditional forecast regime
             end
         else
-            n_fcast_reg -= (get_setting(m, :reg_post_conditional_end)) - # Remove number of regimes switched since starting
+            n_fcast_reg -= (get_setting(m, :reg_post_conditional_end) - 1) - # Remove number of regimes switched since starting
                 get_setting(m, :reg_forecast_start)                    # the conditional forecast
             if n_fcast_reg == 0 # Then no new regimes after conditional forecast ends
                 n_fcast_reg = 1 # So we use the last conditional forecast regime
@@ -285,10 +285,10 @@ function forecast(m::AbstractDSGEModel, system::RegimeSwitchingSystem{S}, z0::Ve
     # conditional forecast regimes, if applicable
     if :obs_nominalrate in cond_full_names(m)
         reg_fcast_cond_start = (cond_type == :none) ? get_setting(m, :reg_forecast_start) :
-            max(get_setting(m, :reg_forecast_start), get_setting(m, :reg_post_conditional_end) - 1)
+            max(get_setting(m, :reg_forecast_start), get_setting(m, :reg_post_conditional_end))
     else
         reg_fcast_cond_start = (cond_type == :none) ? get_setting(m, :reg_forecast_start) :
-            max(get_setting(m, :reg_forecast_start), get_setting(m, :reg_post_conditional_end))
+            max(get_setting(m, :reg_forecast_start), get_setting(m, :reg_post_conditional_end) - 1)
     end
     if n_fcast_reg < get_setting(m, :n_regimes) - reg_fcast_cond_start + 1
         @warn "Number of forecast regimes was less than regimes - reg_fcast_cond_start + 1"
